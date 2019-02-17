e2ca3bd90c4503cff726ef27ec12ca876685c386
Trod010 <Tmrod1318@gmail.com>
2019-02-17 07:34:34 -0500

    Add project files.

---
+++ Calorie Counter/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calorie_Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
