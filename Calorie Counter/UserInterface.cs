6da18a01a97033a7fe2668ba922a19716b7d902b
TRod010 <tmrod1318@gmail.com>
2019-04-23 15:23:09 -0400

    Edit 8

---
+++ Calorie Counter/UserInterface.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calorie_Counter
+{
+    class UserInterface
+    {
+        private StoredData storedData;
+
+        public UserInterface()
+        {
+            storedData = new StoredData();
+        }
+
+        public void AddUser()
+        {
+            Console.WriteLine("Hi there. Can you tell me your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What is your sex? Are you female or male?");
+            string gender = Console.ReadLine();
+            Console.WriteLine("How old are you?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How tall are you in inches?");
+            int height = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How much do you weigh in pounds?");
+            int weight = Convert.ToInt32(Console.ReadLine());
+            storedData.AddUser(name, gender, age, height, weight);
+        }
+
+
+
+    }
+}
